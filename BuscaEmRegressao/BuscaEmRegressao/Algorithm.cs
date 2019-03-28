f7d465446975c92750268114ee0f9630d23716ff
Naate Johnny <naate1996@gmail.com>
2019-03-28 00:32:12 -0300

    Versão final do algoritmo BackTracking da disciplina de IA.

--- BuscaEmRegressao/BuscaEmRegressao/Algorithm.cs
+++ BuscaEmRegressao/BuscaEmRegressao/Algorithm.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BuscaEmRegressao
 {
     public class Algorithm
     {
-        Stack<List<String>> LE, LNE;
-        List<List<String>> BSS, Children;
-        List<String> EC;
+        Stack<List<string>> LE, LNE;
+        List<List<string>> BSS, Children;
+        List<string> EC;
         Int16 CountNodesVisited;
         Boolean Success;
 
@@ -19,7 +20,7 @@ namespace BuscaEmRegressao
             Success = false;
         }
 
-        public void BackTracking (List<String> initialState, List<String> finalState)
+        public void BackTracking (List<string> initialState, List<string> finalState)
         {
             LE.Push(initialState);
             LNE.Push(initialState);
@@ -28,10 +29,11 @@ namespace BuscaEmRegressao
             while (!LNE.Count.Equals(0))
             {
                 CountNodesVisited++;
+                //Console.WriteLine(CountNodesVisited);
 
-                if (EC.Equals(finalState))
+                if (Enumerable.SequenceEqual(EC.OrderBy(t => t), finalState.OrderBy(t => t)))
                 {
-                    foreach (List<String>actual in LE)
+                    foreach (List<string>actual in LE)
                     {
                         ExibirEstado(actual);
                     }
@@ -61,9 +63,10 @@ namespace BuscaEmRegressao
                     }
                     else
                     {
-                        foreach(List<String> e in Children)
+                        foreach(List<string> e in Children)
                         {
-                            LNE.Push(e);
+                            if (!LNE.Contains(e) && !LE.Contains(e) && !BSS.Contains(e))
+                                LNE.Push(e);
                         }
                         EC = LNE.Peek();
                         LE.Push(EC);
@@ -77,68 +80,68 @@ namespace BuscaEmRegressao
         }
     
 
-        public static void ExibirEstado(List<String> estados)
+        public static void ExibirEstado(List<string> estados)
         {
-            List<String> estadosFormatados = new List<string>();
-            String estado = "";
+            List<string> estadosFormatados = new List<string>();
+            string estado = "";
 
-            foreach (String e in estados)
+            foreach (string e in estados)
             {
                 estado += e;
             }
 
             Console.WriteLine("---------------------------------------------------------------------------------------------------");
-            estadosFormatados.Add(String.Join("|", Convert.ToString(estado[0]), Convert.ToString(estado[1]), Convert.ToString(estado[2])));
-            estadosFormatados.Add(String.Join("|", Convert.ToString(estado[3]), Convert.ToString(estado[4]), Convert.ToString(estado[5])));
-            estadosFormatados.Add(String.Join("|", Convert.ToString(estado[6]), Convert.ToString(estado[7]), Convert.ToString(estado[8])));
+            estadosFormatados.Add(string.Join("|", Convert.ToString(estado[0]), Convert.ToString(estado[1]), Convert.ToString(estado[2])));
+            estadosFormatados.Add(string.Join("|", Convert.ToString(estado[3]), Convert.ToString(estado[4]), Convert.ToString(estado[5])));
+            estadosFormatados.Add(string.Join("|", Convert.ToString(estado[6]), Convert.ToString(estado[7]), Convert.ToString(estado[8])));
 
             estadosFormatados.ForEach(est => Console.WriteLine(est));
         }
 
-        public List<List<string>> GerarFilhos(List<String> estadoAtual)
+        public List<List<string>> GerarFilhos(List<string> estadoAtual)
         {
 
             int indicePosicaoVazia = estadoAtual.IndexOf("X");
-            List<List<String>> estados = new List<List<String>>();
+            List<List<string>> estados = new List<List<string>>();
 
-            List<String> listaPosicoesAtuais = new List<String>(estadoAtual);
+            List<string> listaPosicoesAtuais = new List<string>(estadoAtual);
 
             switch (indicePosicaoVazia)
             {
                 case 0:
-                    List<String> listaPosicoesP0P1 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP0P3 = new List<String>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP0P1 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP0P3 = new List<string>(listaPosicoesAtuais);
                     AddEstado(estados, estadoAtual, listaPosicoesP0P1, 1);
                     AddEstado(estados, estadoAtual, listaPosicoesP0P3, 3);
 
                     break;
                 case 1:
-                    List<String> listaPosicoesP1P0 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP1P2 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP1P4 = new List<String>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP1P0 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP1P2 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP1P4 = new List<string>(listaPosicoesAtuais);
                     AddEstado(estados, estadoAtual, listaPosicoesP1P0, 0);
                     AddEstado(estados, estadoAtual, listaPosicoesP1P2, 2);
                     AddEstado(estados, estadoAtual, listaPosicoesP1P4, 4);
                     break;
                 case 2:
-                    List<String> listaPosicoesP2P1 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP2P5 = new List<String>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP2P1 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP2P5 = new List<string>(listaPosicoesAtuais);
                     AddEstado(estados, estadoAtual, listaPosicoesP2P1, 1);
                     AddEstado(estados, estadoAtual, listaPosicoesP2P5, 5);
                     break;
                 case 3:
-                    List<String> listaPosicoesP3P0 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP3P4 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP3P6 = new List<String>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP3P0 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP3P4 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP3P6 = new List<string>(listaPosicoesAtuais);
                     AddEstado(estados, estadoAtual, listaPosicoesP3P0, 0);
                     AddEstado(estados, estadoAtual, listaPosicoesP3P4, 4);
                     AddEstado(estados, estadoAtual, listaPosicoesP3P6, 6);
                     break;
                 case 4:
-                    List<String> listaPosicoesP4P1 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP4P3 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP4P5 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP4P7 = new List<String>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP4P1 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP4P3 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP4P5 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP4P7 = new List<string>(listaPosicoesAtuais);
                     AddEstado(estados, estadoAtual, listaPosicoesP4P1, 1);
                     AddEstado(estados, estadoAtual, listaPosicoesP4P3, 3);
                     AddEstado(estados, estadoAtual, listaPosicoesP4P5, 5);
@@ -146,37 +149,38 @@ namespace BuscaEmRegressao
 
                     break;
                 case 5:
-                    List<String> listaPosicoesP5P2 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP5P4 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP5P8 = new List<String>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP5P2 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP5P4 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP5P8 = new List<string>(listaPosicoesAtuais);
                     AddEstado(estados, estadoAtual, listaPosicoesP5P2, 2);
                     AddEstado(estados, estadoAtual, listaPosicoesP5P4, 4);
                     AddEstado(estados, estadoAtual, listaPosicoesP5P8, 8);
                     break;
                 case 6:
-                    List<String> listaPosicoesP6P3 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP6P7 = new List<String>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP6P3 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP6P7 = new List<string>(listaPosicoesAtuais);
                     AddEstado(estados, estadoAtual, listaPosicoesP6P3, 3);
                     AddEstado(estados, estadoAtual, listaPosicoesP6P7, 7);
                     break;
                 case 7:
-                    List<String> listaPosicoesP7P4 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP7P6 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP7P8 = new List<String>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP7P4 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP7P6 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP7P8 = new List<string>(listaPosicoesAtuais);
                     AddEstado(estados, estadoAtual, listaPosicoesP7P4, 4);
                     AddEstado(estados, estadoAtual, listaPosicoesP7P6, 6);
                     AddEstado(estados, estadoAtual, listaPosicoesP7P8, 8);
 
                     break;
                 case 8:
-                    List<String> listaPosicoesP8P5 = new List<String>(listaPosicoesAtuais);
-                    List<String> listaPosicoesP8P7 = new List<String>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP8P5 = new List<string>(listaPosicoesAtuais);
+                    List<string> listaPosicoesP8P7 = new List<string>(listaPosicoesAtuais);
                     AddEstado(estados, estadoAtual, listaPosicoesP8P5, 5);
                     AddEstado(estados, estadoAtual, listaPosicoesP8P7, 7);
                     break;
 
-                //default:
-                //    Console.WriteLine( string.Empty);
+                default:
+                    Console.WriteLine(string.Empty);
+                    break;
             }
 
             if (estados.Count.Equals(0))
@@ -185,11 +189,11 @@ namespace BuscaEmRegressao
             return estados;
         }
 
-        public void AddEstado(List<List<String>> estados, List<String> estadoAtual, List<String> listaPosicoes,
+        public void AddEstado(List<List<string>> estados, List<string> estadoAtual, List<string> listaPosicoes,
                 int position)
         {
             Swap(listaPosicoes, estadoAtual.IndexOf("X"), position);
-            List<String> estado = new List<String>(listaPosicoes);
+            List<string> estado = new List<string>(listaPosicoes);
 
             if (!LNE.Contains(estado) && !LE.Contains(estado) && !BSS.Contains(estado))
                 estados.Add(estado);
--- BuscaEmRegressao/ExecBackTracking.Cli/Program.cs
+++ BuscaEmRegressao/ExecBackTracking.Cli/Program.cs
@@ -9,11 +9,13 @@ namespace ExecBackTracking.Cli
         static void Main(string[] args)
         {
             List<String> posInicial = new List<String>(new []{"1", "2", "3", "4", "5", "6", "7", "8", "X"});
-            List<String> posFinal = new List<String>(new []{"1", "2", "3", "4", "5", "6", "X", "7", "8" });
+            List<String> posFinal = new List<String>(new []{"1", "2", "3", "4", "X", "5", "6", "7", "8" });
 
             Algorithm algorithm = new Algorithm();
 
             algorithm.BackTracking(posInicial, posFinal);
+
+            Console.ReadKey();
         }
     }
 }
